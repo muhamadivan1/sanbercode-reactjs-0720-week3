8ab9f90108114a44d54009cf6b67b5dcd6eb8aaf
Muhamad Ivan Fadilah <muhammadivan174@gmail.com>
2020-08-05 07:54:59 +0700

    update tugas12

--- my-apps/src/App.js
+++ my-apps/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
 import UserInfo from './tugas11/TabelBuah'
+import Timer from './tugas12/Timer'
+import Clock from './tugas12/Clock'
 
 function App() {
   return (
     <div>
       {/* tugas 1 */}
       <UserInfo/>
+      {/* tugas 2 */}
+      <Timer start={120}/>
+      <Clock/>
     </div>
   );
 }
